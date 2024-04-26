13334692e46cee88923b60d1f3bf9d527132f165
Zichen <hello@withzichen.com>
2024-04-26 13:29:22 -0700

    Developmental repository code cleanup

--- src/vectors.hpp
+++ src/vectors.hpp
@@ -7,24 +7,17 @@
 #include <vector>
 #include <cstdint>
 
-std::vector<uint8_t> int_to_vec(uint32_t integer);
+std::vector<uint8_t>
+int_to_vec(const uint32_t& integer);
 
-void perform_addition(std::vector<uint8_t>& num_two,
-                      std::vector<uint8_t>& num_one);
 
-void process_overflow(std::vector<uint8_t>& vector,
-                      const int32_t base = 10);
+std::vector<uint8_t>
+multiply_vectors(const std::vector<uint8_t>& num_one,
+                 const std::vector<uint8_t>& num_two);
 
-void align_vectors(std::vector<uint8_t>& v1,
-                   std::vector<uint8_t>& v2);
 
-void align_vectors(std::vector<uint32_t>& v1,
-                   std::vector<uint32_t>& v2);
-
-void add_vectors(std::vector<uint8_t>& num_one,
-                 std::vector<uint8_t>&& num_two);
-
-std::vector<uint8_t> multiply_vectors(const std::vector<uint8_t>& num_one,
-                                      const std::vector<uint8_t>& num_two);
+std::vector<uint8_t>
+add_vectors(const std::vector<uint8_t>& num_one,
+            const std::vector<uint8_t>& num_two);
 
 #endif // VOPERATORS_HPP
